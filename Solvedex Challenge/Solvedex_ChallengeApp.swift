f5e3db055187c7a1924c36a9019f3e516fbc8636
Nicolás A. Rodríguez <nrq5510@gmail.com>
2024-02-14 12:11:53 -0500

    Initial Commit

---
+++ Solvedex Challenge/Solvedex_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solvedex_ChallengeApp.swift
+//  Solvedex Challenge
+//
+//  Created by Nicolás A. Rodríguez on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Solvedex_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Solvedex ChallengeUITests/Solvedex_ChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Solvedex_ChallengeUITestsLaunchTests.swift
+//  Solvedex ChallengeUITests
+//
+//  Created by Nicolás A. Rodríguez on 14/02/24.
+//
+
+import XCTest
+
+final class Solvedex_ChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
